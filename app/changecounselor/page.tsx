bd06fa9c7fc91e1381e3a600ad995cd76ba3c2f3
Digvijay25182316 <digvijays.edake2002@gmail.com>
2024-06-01 10:31:11 +0530

    commit

--- app/changecounselor/page.tsx
+++ app/changecounselor/page.tsx
@@ -4,22 +4,27 @@ import { SERVER_URL } from "@/Components/config/config";
 
 async function getCounselors() {
   const response = await fetch(`${SERVER_URL}/counselor`);
-  if (response.ok) {
-    const responseData = await response.json();
-    return responseData;
-  } else {
-    const errorData = await response.json();
-    throw errorData;
+  try {
+    if (response.ok) {
+      const responseData = await response.json();
+      return responseData;
+    } else {
+      const errorData = await response.json();
+      throw new Error(errorData);
+    }
+  } catch (error: any) {
+    throw error;
   }
 }
 
 async function page() {
   const response = await getCounselors();
-  return (
-    <div className="w-full">
-      <ChangeForm counselors={response?.content} />
-    </div>
-  );
+  if (response)
+    return (
+      <div className="w-full">
+        <ChangeForm counselors={response?.content} />
+      </div>
+    );
 }
 
 export default page;
